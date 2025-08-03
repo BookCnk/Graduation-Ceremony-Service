66a1fbab64d18871930200d77a35a5d5563ad719
book_cnk_ <bookchanakarn@gmail.com>
2025-08-04 06:07:07 +0700

    Update BASE_SOCKET_URL in graduate-service.ts to remove socket.io path for simplified connection

--- src/services/graduate-service.ts
+++ src/services/graduate-service.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { getGraduateOverviewController } from "../controllers/summary-controllr";
-const BASE_SOCKET_URL = "https://grad.kmutt.ac.th/socket.io";
+const BASE_SOCKET_URL = "https://grad.kmutt.ac.th";
 
 export const getGraduatesByFacultyPaginated = async (
   facultyId: number,
