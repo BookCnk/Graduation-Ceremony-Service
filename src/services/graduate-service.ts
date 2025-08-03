1e5861a25bc15bce3bfbfc47b738b0538a405e72
book_cnk_ <bookchanakarn@gmail.com>
2025-08-04 00:36:53 +0700

    Update BASE_SOCKET_URL in graduate-service.ts to use the production socket URL for improved connectivity

--- src/services/graduate-service.ts
+++ src/services/graduate-service.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { getGraduateOverviewController } from "../controllers/summary-controllr";
-const BASE_SOCKET_URL = "http://10.2.15.114:3002";
+const BASE_SOCKET_URL = "https://grad.kmutt.ac.th/socket.io";
 
 export const getGraduatesByFacultyPaginated = async (
   facultyId: number,
